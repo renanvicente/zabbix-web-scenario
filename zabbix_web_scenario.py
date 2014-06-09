2733b3222676b3acfe5baed926600b1da7936494
renanvicente <renanvice@gmail.com>
2014-06-09 15:30:28 -0300

    upload for the new API version

--- zabbix_web_scenario.py
+++ zabbix_web_scenario.py
@@ -37,20 +37,20 @@ def authentication(server_url,user,password):
     sys.exit(1)
 
 
-def create_web_scenario(self,name,url,group,url_name='Homepage',status='200'):
+def create_web_scenario(self,name,url,group,hostid,url_name='Homepage',status='200'):
   request = ZabbixAPI.do_request(self, 'webcheck.get', params={ "filter": {"name": name}})
   if request['result']:
     print('Host "%s" already registered' % name)
     sys.exit(1)
   else:
     try:
-      ZabbixAPI.do_request(self, 'webcheck.create',params={"name": name,"applicationid": '4761',"delay": '60',"retries": '3', "steps": [ { 'name': url_name, 'url': url,'status_codes': status, 'no': '1'} ] } )
+      ZabbixAPI.do_request(self, 'webcheck.create',params={"name": name,"hostid": hostid, "delay": '60',"retries": '3', "steps": [ { 'name': url_name, 'url': url,'status_codes': status, 'no': '1'} ] } )
       triggers = create_trigger(auth,name,url,group)
     except Exception, e:
       print(e)
       sys.exit(1)
 
-def create_by_file(auth, group, filename):
+def create_by_file(auth, group, hostid, filename):
   try:
     file_to_parse = open(filename,'r')
     try:
@@ -67,9 +67,9 @@ def create_by_file(auth, group, filename):
         except IndexError:
           url_name = None
         if url_name:
-          create_web_scenario(auth,name,url,group,url_name)
+          create_web_scenario(auth,name,url,group,hostid,url_name)
         else:
-          create_web_scenario(auth,name,url,group)
+          create_web_scenario(auth,name,url,group,hostid)
     finally:
       file_to_parse.close()
   except IOError:
@@ -92,21 +92,22 @@ if __name__ == '__main__':
   parser.add_option('-p','--password',dest='password',help='Password for authentication',metavar='PASSWORD')
   parser.add_option('-f','--file',dest='filename',help='File with Name,URL',metavar='FILE')
   parser.add_option('-g','--group-name',dest='group',help='Host Group Name',metavar='GROUP')
+  parser.add_option('-i','--host-id',dest='hostid',help='Host ID',metavar='HOSTID')
   (options, args) = parser.parse_args()
   auth = authentication(options.server_url,options.user,options.password)
   if options.filename:
-    create_by_file(auth, options.group, options.filename)
+    create_by_file(auth, options.group, options.hostid, options.filename)
   else:
     if not options.group:
       print('Group must be required')
       sys.exit(1)
     if options.status:
       if options.url_name:
-        web_scenario = create_web_scenario(auth, options.name,options.url,options.group, options.url_name,options.status)
+        web_scenario = create_web_scenario(auth, options.name,options.url,options.group, options.hostid, options.url_name,options.status)
       else:
-        web_scenario = create_web_scenario(auth, options.name,None,options.url, options.group, options.status)
+        web_scenario = create_web_scenario(auth, options.name,None,options.url, options.group, options.hostid, options.status)
     else:
       if options.url_name:
-        web_scenario = create_web_scenario(auth, options.name,options.url, options.group, options.url_name)
+        web_scenario = create_web_scenario(auth, options.name,options.url, options.group, options.hostid, options.url_name)
       else:
-        web_scenario = create_web_scenario(auth, options.name,options.url, options.group)
+        web_scenario = create_web_scenario(auth, options.name,options.url, options.group, options.hostid)
